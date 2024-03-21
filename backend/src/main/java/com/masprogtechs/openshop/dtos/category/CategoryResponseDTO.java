f78c9a00b2b8fbd4dade43fe3fe0960517daca6f
masprog2022 <masprog2022@gmail.com>
2024-03-21 23:09:06 +0100

    feat: create CategoryResponseDTO

--- backend/src/main/java/com/masprogtechs/openshop/dtos/category/CategoryResponseDTO.java
+++ backend/src/main/java/com/masprogtechs/openshop/dtos/category/CategoryResponseDTO.java
@@ -1,9 +1,16 @@
 package com.masprogtechs.openshop.dtos.category;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
-@Data
-public class CategoryRequestDTO {
+import java.time.LocalDateTime;
 
+@Data
+public class CategoryResponseDTO {
+
+    private Long id;
     private String name;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDateTime createdAt;
 }

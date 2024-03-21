290396cb8f0ab3863699047eb6b4e3b6a771c141
masprog2022 <masprog2022@gmail.com>
2024-03-21 21:54:59 +0100

    feat: create CategoryRepository

--- backend/src/main/java/com/masprogtechs/openshop/repositories/CategoryRepository.java
+++ backend/src/main/java/com/masprogtechs/openshop/repositories/CategoryRepository.java
@@ -1,2 +1,8 @@
-package com.masprogtechs.openshop.repositories;public interface CategoryRepository {
+package com.masprogtechs.openshop.repositories;
+
+import com.masprogtechs.openshop.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
 }
